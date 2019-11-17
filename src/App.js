8f6e9073fb3011dae0068e1285495cc1add50272
RNm-dove <m.rynk.plume@gmail.com>
2019-11-17 21:58:39 +0900

    master color select?

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import React, { Component } from 'react';
+import { Stage, Layer, Line, Rect, Text } from 'react-konva';
 import Konva from 'konva';
 
 class ColoredRect extends React.Component {
   state = {
-    color: 'green'
+    color: 'red'
   };
   handleClick = () => {
+    let color = Konva.Util.getRandomColor()
     this.setState({
-      color: Konva.Util.getRandomColor()
+      color: color
     });
+    // 親コンポーネントに値渡し
+    this.props.onClick(color)
   };
   render() {
     return (
@@ -26,19 +29,72 @@ class ColoredRect extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Text text="Try click on rect" />
-            <ColoredRect />
-          </Layer>
-        </Stage>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    lines: [],
+    color: 'red'
+  };
+
+  handleMouseDown = () => {
+    this._drawing = true;
+    // add line
+    this.setState({
+      lines: [...this.state.lines, []]
+    });
+  };
+
+  handleMouseMove = e => {
+    // no drawing - skipping
+    if (!this._drawing) {
+      return;
+    }
+    const stage = this.stageRef.getStage();
+    const point = stage.getPointerPosition();
+    const { lines } = this.state;
+
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine = lastLine.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    this.setState({
+      lines: lines.concat()
+    });
+  };
+
+  handleMouseUp = () => {
+    this._drawing = false;
+  };
+
+  handleClick = color => {
+    this.setState({ color: color });
+  }
+
+  render() {
+    return (
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onContentMousedown={this.handleMouseDown}
+        onContentMousemove={this.handleMouseMove}
+        onContentMouseup={this.handleMouseUp}
+        ref={node => {
+          this.stageRef = node;
+        }}
+      >
+        <Layer>
+          <Text text="何か書いてもいいよ" />
+          <ColoredRect
+            onClick={color => this.handleClick(color)}
+           />
+          {this.state.lines.map((line, i) => (
+            <Line key={i} points={line} stroke={this.state.color} />
+          ))}
+        </Layer>
+      </Stage>
+    );
+  }
 }
 
 export default App;
